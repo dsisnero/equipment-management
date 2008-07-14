fcb42ced5fffe49b423a8ab1b7122712bc1315d0
Dominic Sisneros <dsisnero@gmail.com>
2008-07-14 16:47:15 -0600

    added flash_for as an application helper

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,9 +2,19 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+
+    include AuthenticatedSystem
+
+  #before_filter :find_site
   helper :all # include all helpers, all the time
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '0690c9554d3134d52b9fd3e7de96e05b'
+
+  protected
+
+  def find_site
+     @site = Site.find_by_lid_fac(params[:site_id])
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def flash_for(flash_class)
+    sym = flash_class.intern
+    if flash[sym]
+      content_tag('div', h(flash[sym]), :class => flash_class , :id => flash_class)
+    end
+  end
 end
