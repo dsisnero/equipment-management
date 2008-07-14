72ea91080753113114a51cf26c81c1769a682f3c
Dominic Sisneros <dsisnero@gmail.com>
2008-07-14 16:52:29 -0600

    added resource routes and got rid of example routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,71 @@
 ActionController::Routing::Routes.draw do |map|
-  # The priority is based upon order of creation: first created -> highest priority.
+  map.resources :users, :member => { :suspend   => :put,
+    :unsuspend => :put,
+    :purge     => :delete }
 
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
 
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
+  map.resource :session
 
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
+  map.resources :patch_panels
 
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
-  # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
-  #   end
+  map.resources :equipment
 
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
 
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.resources :rooms
 
-  # See how all your routes lay out with "rake routes"
+  map.resources :sites do |site|
+    site.resources :racks
+    site.resources :devices
+    site.resources :buildings do |building|
+      building.resources :floors
+      building.resources :rooms do |room|
+        room.resources :racks
+      end
+    end
+  end
 
-  # Install the default routes as the lowest priority.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
-end
+
+# The priority is based upon order of creation: first created -> highest priority.
+
+# Sample of regular route:
+#   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
+# Keep in mind you can assign values other than :controller and :action
+
+# Sample of named route:
+#   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
+# This route can be invoked with purchase_url(:id => product.id)
+
+# Sample resource route (maps HTTP verbs to controller actions automatically):
+#   map.resources :products
+
+# Sample resource route with options:
+#   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
+
+# Sample resource route with sub-resources:
+#   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
+
+# Sample resource route with more complex sub-resources
+#   map.resources :products do |products|
+#     products.resources :comments
+#     products.resources :sales, :collection => { :recent => :get }
+#   end
+
+# Sample resource route within a namespace:
+#   map.namespace :admin do |admin|
+#     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
+#     admin.resources :products
+#   end
+
+# You can have the root of your site routed with map.root -- just remember to delete public/index.html.
+map.root :controller => "sites"
+
+# See how all your routes lay out with "rake routes"
+
+# Install the default routes as the lowest priority.
+#map.connect ':controller/:action/:id'
+#map.connect ':controller/:action/:id.:format'
+  end
